2aeb8597196af29aef385f01c02d24189292c7ba
Mathieuhuot89 <100226494+Mathieuhuot89@users.noreply.github.com>
2022-12-13 09:24:32 +0100

    Add files via upload

---
+++ core/scripts/main.js
@@ -0,0 +1,17 @@
+let name = 'Mathieu';
+
+let age = 18;
+
+let drive = 'You can drive';
+
+let notDrive = false;
+
+const cars = ['mercedes', 'bmw', 'ferrari']
+
+if (age >= 18) {
+    console.log(drive)
+} else {
+    console.log(notDrive)
+}
+
+console.log('Bonjour je m\'appelle ');
